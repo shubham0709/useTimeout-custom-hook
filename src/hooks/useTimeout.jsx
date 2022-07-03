e3d8c08028732034cafba9e4819d6c3d2b5cb954
Shubham Barore <shubham07barore@gmail.com>
2022-07-03 22:10:28 +0530

    implemented use timeput custom hook

---
+++ src/hooks/useTimeout.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+const useTimeout = (delay) => {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setReady(true);
+    }, delay);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return ready;
+};
+
+export { useTimeout };
